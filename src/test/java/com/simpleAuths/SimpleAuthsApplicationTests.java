6dcf5962ae6d8466aede2fd55b39af91879f431f
evan0120-yo <evan01203394@gmail.com>
2024-07-19 09:48:01 +0800

    first commit

---
+++ src/test/java/com/simpleAuths/SimpleAuthsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleAuths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAuthsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
